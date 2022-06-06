b59dd6309cf99f9104bf9d8095a905a834663727
Mohammad Salehi <mds1121@yahoo.com>
2022-06-06 22:44:49 +0430

    set default value

--- database/migrations/2022_06_06_090652_create_orders_table.php
+++ database/migrations/2022_06_06_090652_create_orders_table.php
@@ -16,7 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedTinyInteger('status')->default(config('constants.orderStatus.pending'));
-            $table->string('address');
+            $table->string('address')->default('');
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
