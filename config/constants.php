52dfa1f9f5e9f312427df2bb2fc2280339ca24ea
Mohammad Salehi <mds1121@yahoo.com>
2022-06-06 19:51:31 +0430

    updat eroutes

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+   'orderStatus' => [
+      'pending' => '1',
+      'processing' => '2',
+      'completed' => '3',
+      'cancelled' => '4'
+   ]
+];
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FoodController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/category/{category:slug}',[FoodController::class, 'category'])->name('food.category');
+Route::get('/food/{food}',[FoodController::class, 'show'])->name('food.show');
+
+Route::post('order/add/{food}',[OrderController::class, 'add'])->name('order.add');
