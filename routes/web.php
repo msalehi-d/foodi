2c3875a64dae5f7a3ac4df5f847ec152692128f4
MohammadSalehi <mds1121@yahoo.com>
2022-06-07 17:25:50 +0430

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\OrderController as AdminOrderController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\Customer\OrderController as CustomerOrderController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\FoodController as AdminFoodController;
+use App\Http\Controllers\Customer\OrderController;
 use App\Http\Controllers\FoodController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,13 +27,21 @@ Route::get('/category/{category:slug}', [FoodController::class, 'category'])->na
 
 Route::get('/food/{food}', [FoodController::class, 'show'])->name('food.show');
 
+Route::name('admin.')->prefix('admin')->middleware(['auth'])->group(function () {
+    Route::get('orders', [AdminOrderController::class, 'index'])->name('order');
+    Route::patch('order/{order}/accept', [AdminOrderController::class, 'accept'])->name('order.accept');
+    Route::patch('order/{order}/reject', [AdminOrderController::class, 'reject'])->name('order.reject');
 
+    Route::resource('category', CategoryController::class);
+    Route::resource('food', AdminFoodController::class);
+});
 Route::name('customer.')->prefix('customer')->middleware(['auth'])->group(function () {
     Route::post('order/add/{food}', [OrderController::class, 'add'])->name('order.add');
     Route::get('order/show', [OrderController::class, 'show'])->name('order.show');
     Route::post('order/{order}/submit', [OrderController::class, 'submit'])->name('order.submit');
     Route::patch('order/{order}/update', [OrderController::class, 'update'])->name('order.update');
-    
-    Route::get('order/index', [CustomerOrderController::class, 'index'])->name('order.index');
-    Route::get('account', [CustomerOrderController::class, 'index'])->name('account');
+    Route::get('order/{order}/success', [OrderController::class, 'success'])->name('order.success');
+
+    Route::get('order/index', [OrderController::class, 'index'])->name('order.index');
+    Route::get('account', [OrderController::class, 'index'])->name('account');
 });
